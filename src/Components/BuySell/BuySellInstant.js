995a39c58190623f6f0556972ca7de41a42bc2dc
eurocodes <emmanuelumeoke@gmail.com>
2020-12-01 19:33:33 +0100

    Added buy sell intantly page

--- src/Components/BuySell/BuySellInstant.js
+++ src/Components/BuySell/BuySellInstant.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './buysell.css';
 import Layout from '../Layout/index';
 
 export const BuySellInstant = (props) => {
+    const [buySellBtn, setBuySellBtn] = useState({
+        buy: true,
+        sell: false,
+    })
+    const toBuy = () => {
+        setBuySellBtn({
+            buy: true,
+            sell: false,
+        })
+    }
+    const toSell = () => {
+        setBuySellBtn({
+            buy: false,
+            sell: true,
+        })
+    }
     let BrowserRoter = props.history.push
     let CurrentRoute = props.location.pathname
     return (
@@ -14,10 +30,23 @@ export const BuySellInstant = (props) => {
                         <p>A simple way to buy or sell cryptocurrency in less than a minute</p>
                     </div>
                     <div className="buy-sell-btn-container">
-                        <button className="button-toggle button-buy"
-                            style={{ backgroundColor: props.focused ? "#307ccc" : "#fff" }}
+                        <button
+                            onClick={() => toBuy()}
+                            className="button-toggle button-buy"
+                            style={{
+                                backgroundColor: buySellBtn.buy ? "#ffb500" : "#fff",
+                                color: buySellBtn.buy ? "#fff" : ""
+                            }}
                         >Buy</button>
-                        <button className="button-toggle button-sell">Sell</button>
+                        <button
+                            onClick={() => toSell()}
+                            className="button-toggle button-buy"
+                            style={{
+                                backgroundColor: buySellBtn.sell ? "#ffb500" : "#fff",
+                                color: buySellBtn.sell ? "#fff" : ""
+                            }}
+                            className="button-toggle button-sell"
+                        > Sell</button>
                     </div>
                     <div className="buy-sell-input-container">
                         <div>
@@ -36,6 +65,25 @@ export const BuySellInstant = (props) => {
                                 </select>
                             </div>
                         </div>
+                    </div>
+                    <div className="buy-sell-input-container">
+                        <div>
+                            <h6>Amount to spend</h6>
+                            <div className="input-feild-1">
+                                <div className="currency-holder">NGN</div>
+                                <input name="amount" className="amount-holder" placeholder="Amount" />
+                            </div>
+                            <div className="description">
+                                <h6>Minimum amount: NGN100.00</h6>
+                                <h6>Balance: NGN0.00</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="input-note">
+                        <input name="buysellnote" placeholder="Note (Optional)" />
+                    </div>
+                    <div className="submit-btn">
+                        <button onClick={() => { }}>Continue</button>
                     </div>
                 </div>
             </div>
