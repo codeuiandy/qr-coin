b944c2a3e3b02f04f9942c7a87869cf6f6c5c9b0
Code Ui Andy <43136310+codeuiandy@users.noreply.github.com>
2020-12-07 15:28:19 +0100

    updates

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import {useRecoilState} from 'recoil'
+import {toggleSidebar} from '../../globalState/localData'
+import Avatar from '../../Assets/avatar.png'
 import Navbar from './Navbar'
 import Sidebar from './Sidebar'
 import './layout.css'
 export default function Index(props) {
+    let [sidebarState, setSidebarState] = useRecoilState(toggleSidebar)
     const [LastScrool, setLastScrool] = useState(0)
     const [ticking, setticking] = useState(false)
     const [Hidenav, setHideNav] = useState(false)
@@ -15,7 +19,7 @@ export default function Index(props) {
 
     return (
         <React.Fragment>
-            <div >
+            <div>
                 <div id="hideNav">
                     <Navbar BrowserRoter={props.BrowserRoter} />
                 </div>
@@ -26,7 +30,7 @@ export default function Index(props) {
 
                 </div>
                 <div>
-                    <section className="app-container">
+                    <section onClick={()=>setSidebarState({...sidebarState, openSidebar:false})}  className="app-container">
                         {props.children}
                     </section>
 
